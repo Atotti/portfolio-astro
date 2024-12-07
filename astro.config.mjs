9e9c839f2e03670507a62587f225e72493fb2007
Ayuto Tsutsumi <56724676+Atotti@users.noreply.github.com>
2024-12-07 14:37:21 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Atotti.github.io/astro-portfolio',
-  base: '/astro-portfolio',
+  site: 'https://Atotti.github.io/portfolio-astro',
+  base: '/portfolio-astro',
   outDir: 'dist',
 })
